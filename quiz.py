cd1039a79909caa2f8a5b20ca3c6afa1d597db71
caoimhe daly <keve87@hotmail.com>
2018-10-11 15:59:59 +0000

    score added

--- quiz.py
+++ quiz.py
@@ -1,7 +1,11 @@
-def show_menu():
 
-    print ("Quiz game")
-    print ("xxxxxxxxxx")
+
+
+def show_menu():
+    print()
+    print("XXXXXXXXXXXX")
+    print ("XXQuiz gameXX")
+    print ("XXXXXXXXXXXX")
     print()
     print("1. Run Quiz")
     print("2. Enter a question")
@@ -21,10 +25,38 @@ def add_a_question():
     with open("questions.txt", "a") as f:
         line =  question + "|" + answer + "\n"
         f.write(line)
-  
+        
+
+
+def ask_questions():   
     
+    print("\n Let's run the Quiz! \n")
+    
+    with open("questions.txt") as f:
+        
+        questions = f.read().split("\n")[:-1]
+        
+        
+    score = 0
+    
+    for q in questions:
+        split =  q.split("|")
+        Q = (split[0])
+
+        Ans = (split[1])
+
+        guess = input(Q)
+        print(guess)
+        
+        if guess == Ans:
+            score +=1
+            
+    print("you scored {0}".format(score))
+
 
     
+
+
 while True:
     
     option = show_menu()
@@ -34,7 +66,8 @@ while True:
         break
         
     if option == "1":
-        print("\n Let's run the Quiz! \n")
+        ask_questions()
+      
     
     if option == "2":
         add_a_question()
