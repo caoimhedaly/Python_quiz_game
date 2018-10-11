778c026d07d9852850696c4bf240bd9bd0c9ef53
caoimhe daly <keve87@hotmail.com>
2018-10-11 14:02:48 +0000

    exit working

--- quiz.py
+++ quiz.py
@@ -1,9 +1,20 @@
-print ("Quiz game")
-print ("xxxxxxxxxx")
-print()
-print("1. Run Quiz")
-print("2. Enter a question")
-print("3. Exit")
-print()
+def show_menu():
 
-print = input("Enter option: ")
+    print ("Quiz game")
+    print ("xxxxxxxxxx")
+    print()
+    print("1. Run Quiz")
+    print("2. Enter a question")
+    print("3. Exit")
+    print()
+
+    option= input("Enter option: ")
+
+    return option
+    
+while True:
+    
+    option = show_menu()
+    
+    if option == "3":
+        break
