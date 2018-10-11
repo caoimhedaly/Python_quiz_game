46d9c4ee03391478ba895e426fc9f1f6065998ff
caoimhe daly <keve87@hotmail.com>
2018-10-11 15:04:23 +0000

    question working

--- quiz.py
+++ quiz.py
@@ -12,9 +12,30 @@ def show_menu():
 
     return option
     
+    
+def add_a_question():
+
+    question = input("enter a question: ")
+    answer = input("You tell me, " + question.lower()+ ": ")
+    
+    with open("questions.txt", "a") as f:
+        line =  question + "|" + answer + "\n"
+        f.write(line)
+  
+    
+
+    
 while True:
     
     option = show_menu()
     
     if option == "3":
+        print("\n Goodbye :(")
         break
+        
+    if option == "1":
+        print("\n Let's run the Quiz! \n")
+    
+    if option == "2":
+        add_a_question()
+    
